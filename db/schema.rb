5b03a15ee8131df68e9b52c7490c6e1b49759394
KIKU <kikuno-yuki@av.avex.co.jp>
2016-10-24 06:01:49 +0000

    userにカラム追加する

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023145025) do
+ActiveRecord::Schema.define(version: 20161024052525) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name",       null: false
@@ -54,6 +54,8 @@ ActiveRecord::Schema.define(version: 20161023145025) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "family_name"
+    t.string   "given_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
