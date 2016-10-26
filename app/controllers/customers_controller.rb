d96815e5b75ee8884c834423724e4a04b7c68e5d
KIKU <kikuno-yuki@av.avex.co.jp>
2016-10-26 05:32:26 +0000

    09_基本課題1

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,6 @@
 class CustomersController < ApplicationController
   
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!
   before_action :set_customers, only: [:edit, :update, :show, :destroy]
   
   def index
